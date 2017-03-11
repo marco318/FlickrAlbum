bc6ab1a760e9e65678baa29c0db14bc34f7655b2
Mark <mhkim318@gmail.com>
2017-03-11 12:14:22 +0900

    Add a new view: AlbumDisplayViewController, which follows AlbumDisplay protocol

---
+++ FlickrAlbum/View/AlbumDisplayViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AlbumDisplayViewController.swift
+//  FlickrAlbum
+//
+//  Created by kimmyunghoon on 2017. 3. 11..
+//  Copyright © 2017년 Raspberry Soft. All rights reserved.
+//
+
+import Foundation
+
+class AlbumDisplayViewController: ViewController {
+  
+}
+
+extension AlbumDisplayViewController: AlbumDisplay {
+  func fetch(next: Photo) {
+
+  }
+  
+  func show(next: Photo) {
+    
+  }
+  
+  func notifyDidFinishDisplay(of old: Photo) {
+    
+  }
+}
